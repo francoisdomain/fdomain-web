4cd428c3b39b423f57789cac29c4af245257883c
francoisdomain <51420342+francoisdomain@users.noreply.github.com>
2025-03-01 18:36:05 +0000

    Update translations.ts

--- src/utils/translations.ts
+++ src/utils/translations.ts
@@ -165,7 +165,7 @@ export const translations: TranslationType = {
     "sections": {
       "featuredBooks": "En vedette",
       "aboutTitle": "L'auteur",
-      "aboutText": "Dans des textes à fleur de peau, Domain ausculte et questionne ces miroirs sans tain à travers lesquels on se construit, ces frontières hypothétiques derrière lesqueles on s'isole de notre plein gré. Sans filtre et sans concession, les romans de François Domain dépouillent l'âme de son vernis et l'exposent, nue et vulnérable.",
+      "aboutText": "Dans des textes à fleur de peau, Domain ausculte et questionne ces miroirs sans tain à travers lesquels on se construit, ces frontières hypothétiques où l'on s'abrite. Sans filtre et sans concession, les romans de François Domain traquent l'ambiguité, questionnent l'identité, dissèquent les relations humaines — en particulier celle de l'homme face à lui-même.",
       "contactTitle": "Contacter François Domain",
       "contactText": "Pour plus d'information, séances de dédicaces ou simplement pour dire bonjour."
     },
