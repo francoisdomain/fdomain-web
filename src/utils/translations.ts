7ab19431f1da0650ccf9e4d08b8617811c410dac
francoisdomain <51420342+francoisdomain@users.noreply.github.com>
2025-03-02 23:24:08 +0000

    Update translations.ts

--- src/utils/translations.ts
+++ src/utils/translations.ts
@@ -82,7 +82,7 @@ export const translations: TranslationType = {
   "en": {
     "hero": {
       "title": "Blurring the lines that define us.",
-      "subtitle": "Beyond the lies that reassure and the truths we ignore, the shadow of the self lingers."
+      "subtitle": "Beyond the lies that reassure and the truths we ignore lingers the shadow of the self."
     },
     "nav": {
       "about": "The Author",
