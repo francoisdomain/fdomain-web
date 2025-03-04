7d86242746dff5bc5301e44c1631d19b234bdc3b
francoisdomain <51420342+francoisdomain@users.noreply.github.com>
2025-03-04 21:18:53 +0000

    Update blogArticles.ts

--- src/data/blogArticles.ts
+++ src/data/blogArticles.ts
@@ -26,7 +26,7 @@ We can learn tricks on how to inspire people, we can learn how to drive a team, 
 Leadership isn’t about the *how*—it’s about the *why*.
 
 No one becomes a leader just by giving it a go. 
-But <b>anyone</b> can become a leader when they <strong>find their purpose</strong>.
+But **anyone** can become a leader when they <strong>find their purpose</strong>.
 
 💡 **Leadership isn’t something you learn—it’s something you stumble upon.**
 
@@ -41,13 +41,41 @@ Looking back at history, what did the great leaders of this world have in common
     imageUrl: "/img/blog/EveryoneLeader.webp",
     publishedDate: "2025-03-04"
   },
-  "lead-and-inspire": {
+  "lead-vs-inspire": {
     id: 2,
     title: "Can you lead without inspiring?",
-    slug: "lead-and-inspire",
+    slug: "lead-vs-inspire",
     summary: "You can inspire without leading - but can you lead without inspiring?",
     body: `
-comgin up
+    🔥 And here comes the buzzword: **VISION**.
+    
+    No word has been more overused—or misunderstood. “I want to increase my bottom line by 40%” has very little to do with *Vision*—it’s merely an objective.
+    
+    The distinction is the same as *management* vs. *inspiration*.
+    ✅ A good manager will give you a target—and hopefully the tools to achieve it.
+    ✅ A leader will make you want to achieve it.
+    
+    And if money isn’t a long-term motivator, **inspiration** is.
+    
+    We often think of leadership and inspiration as interchangeable. They’re not.
+    We can inspire someone—with words, actions, or ideas—without leading them anywhere. And that’s valuable. It can change the way they think, the way they behave. But it doesn’t necessarily drive action.
+    
+    💡 **Leadership, however, is *impossible* without inspiration.**
+    
+    Leaders doesn’t just give instructions. They move people. Not with authority, but with meaning.
+    👉 You can manage a team without inspiring them.
+    👉 You can give orders without engagement.
+    👉 You can enforce rules without belief.
+    **But without inspiration, leadership is just administration.**
+    
+    And remember the saying:
+    💡 “People don’t leave jobs—they leave managers.”
+    
+    🎭 *The same applies to storytelling. The most unforgettable characters aren’t the ones who control or command. They’re the ones who **make us feel** something.*
+    
+    So here’s a statement that isn’t as obvious as it sounds:
+    As inspiration can’t be faked or forced upon anyone, there’s only one way to get it right:
+    👉 **Inspire yourself before inspiring others.**
     `,
     imageUrl: "/img/character-development.jpg",
     publishedDate: "2023-06-22"
