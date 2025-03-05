1659d9f01abb9ed092754a7d495fe68374e81a5d
francoisdomain <51420342+francoisdomain@users.noreply.github.com>
2025-03-05 21:39:40 +0000

    Update blogArticles.ts

--- src/data/blogArticles.ts
+++ src/data/blogArticles.ts
@@ -47,37 +47,9 @@ Looking back at history, what did the great leaders of this world have in common
     slug: "lead-vs-inspire",
     summary: "You can inspire without leading - but can you lead without inspiring?",
     body: `
-👉 How many times have you heard (or told yourself), *“I’m not a leader”*?
+    ---
 
-      🔥 And here comes the buzzword: **VISION**.
-      
-      No word has been more overused—or misunderstood. “I want to increase my bottom line by 40%” has very little to do with *Vision*—it’s merely an objective.
-      
-      The distinction is the same as *management* vs. *inspiration*.
-      ✅ A good manager will give you a target—and hopefully the tools to achieve it.
-      ✅ A leader will make you want to achieve it.
-      
-      And if money isn’t a long-term motivator, **inspiration** is.
     
-    We often think of leadership and inspiration as interchangeable. They’re not.
-    We can inspire someone—with words, actions, or ideas—without leading them anywhere. And that’s valuable. It can change the way they think, the way they behave. But it doesn’t necessarily drive action.
-    
-    💡 **Leadership, however, is *impossible* without inspiration.**
-    
-    Leaders doesn’t just give instructions. They move people. Not with authority, but with meaning.
-    👉 You can manage a team without inspiring them.
-    👉 You can give orders without engagement.
-    👉 You can enforce rules without belief.
-    **But without inspiration, leadership is just administration.**
-    
-    And remember the saying:
-    💡 “People don’t leave jobs—they leave managers.”
-    
-    🎭 *The same applies to storytelling. The most unforgettable characters aren’t the ones who control or command. They’re the ones who **make us feel** something.*
-    
-    So here’s a statement that isn’t as obvious as it sounds:
-    As inspiration can’t be faked or forced upon anyone, there’s only one way to get it right:
-    👉 **Inspire yourself before inspiring others.**
     `,
     imageUrl: "/img/blog/lead-vs-inspire.webp",
     publishedDate: "2025-03-12"
