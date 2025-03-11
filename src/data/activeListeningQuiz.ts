4bfdc7b8b22f969847388a6494694f6ae08346db
francoisdomain <51420342+francoisdomain@users.noreply.github.com>
2025-03-11 21:53:51 +0000

    Update activeListeningQuiz.ts

--- src/data/activeListeningQuiz.ts
+++ src/data/activeListeningQuiz.ts
@@ -143,7 +143,7 @@ export const activeListeningQuiz: QuizData = {
       maxScore: 8,
       title: `Oops! Hearing, Not Listening. 🚨`,
       description: 
-        `Oops! Hearing, Not Listening. 🚨`
+        `You might be **physically present** in conversations, but your mind is often elsewhere—scrolling, zoning out, or just waiting for the other person to stop talking. This can lead to **misunderstandings, missed details, and frustrated colleagues** who feel unheard.`
       
     },
     {
