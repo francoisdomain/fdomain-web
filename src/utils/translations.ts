ac8e4498c0c206b05583848b23561e23f719d3d4
francoisdomain <51420342+francoisdomain@users.noreply.github.com>
2025-06-02 22:26:21 -0400

    Update translations.ts

--- src/utils/translations.ts
+++ src/utils/translations.ts
@@ -96,7 +96,7 @@ export const translations: TranslationType = {
       "aboutTitle": "About the Author",
       "aboutText": "Raw and visceral, Domain’s body of work navigates the two-way mirrors through which we shape ourselves and the intangible borders where we seek refuge. He explores ambiguous connections, challenges identity, certainties, and illusions—ultimately confronting the most complex human relationship of all: the one with oneself.",
       "contactTitle": "Get in Touch",
-      "contactText": "François Domain is represented by the Agency Editio Dialog."
+      "contactText": "François Domain is represented by Editio Dialog Literary Agency."
     },
     "buttons": {
       "learnMore": "Learn More",
